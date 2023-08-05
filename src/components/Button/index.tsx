0b1908c9436ae2a963c56e90f7b6c7120182c2fb
Júnior Nascimento <junior.nascm@gmail.com>
2023-08-05 20:52:04 -0300

    feature: simplify Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Match, Switch } from "solid-js";
+import { Component } from "solid-js";
 
 interface ButtonProps {
   label: string;
@@ -7,85 +7,29 @@ interface ButtonProps {
   disabled?: boolean;
   active?: boolean;
 }
-
-interface ButtonVariantProps {
-  label: string;
-  onClick: () => void;
-}
+const baseClass = 'py-[10px] font-bold text-base rounded-lg flex items-center justify-center transition-all select-none w-full md:px-4';
+const primaryClass = "bg-purple text-white hover:opacity-25 cursor-pointer";
+const primaryDeactive = "bg-purple-hover text-white cursor-pointer hover:opacity-25";
+const primaryDisabled = "bg-purple opacity-25 text-white cursor-not-allowed hover:opacity-25";
+const secondaryClass = "text-purple border border-purple cursor-pointer hover:bg-purple-hover";
+const secondaryDesactive = "text-purple border border-purple bg-light-purple cursor-pointer hover:opacity-25";
+const secondaryDisabled = "text-purple opacity-25 border border-purple cursor-not-allowed hover:opacity-25";
 
 const Button: Component<ButtonProps> = (props) => {
+  const style = `${baseClass} 
+  ${props.secondary ? (
+    props.disabled ? secondaryDisabled : props.active ? secondaryDesactive : secondaryClass
+  ) : ''}
+  ${!props.secondary ? (
+    props.disabled ? primaryDisabled : props.active ? primaryDeactive : primaryClass
+  ) : ''}
+  `;
+
   return(
-    <Switch fallback={<ButtonPrimaryDefault label={props.label} onClick={props.onClick}/>}>
-      <Match when={!props.secondary}>
-        <Switch fallback={<ButtonPrimaryDefault label={props.label} onClick={props.onClick}/>}>
-          <Match when={props.disabled}>
-            <ButtonPrimaryDisabled label={props.label} onClick={props.onClick}/>
-          </Match>
-          <Match when={props.active}>
-            <ButtonPrimaryActive label={props.label} onClick={props.onClick}/>
-          </Match>
-        </Switch>
-      </Match>
-      <Match when={props.secondary}>
-        <Switch fallback={<ButtonSecondaryDefault label={props.label} onClick={props.onClick}/>}>
-          <Match when={props.disabled}>
-            <ButtonSecondaryDisabled label={props.label} onClick={props.onClick}/>
-          </Match>
-          <Match when={props.active}>
-            <ButtonSecondaryActive label={props.label} onClick={props.onClick}/>
-          </Match>
-        </Switch>
-      </Match>
-    </Switch>
+    <div class={style} onClick={props.onClick}>
+      <div class="h-6">{props.label}</div>
+    </div>
   )
-};
-
-const ButtonPrimaryDefault: Component<ButtonVariantProps> = (props) => {
-  return (
-    <div class="box-border py-[10px] bg-purple text-white font-bold text-base rounded-lg flex items-center justify-center cursor-pointer hover:opacity-25 transition-all md:px-4 select-none w-full" onClick={props.onClick}>
-      <div class="h-6">{props.label}</div>
-    </div>
-  );
-};
-
-const ButtonPrimaryActive: Component<ButtonVariantProps> = (props) => {
-  return (
-    <div class="box-border py-[10px] bg-purple-hover text-white font-bold text-base rounded-lg flex items-center justify-center cursor-pointer hover:opacity-25 transition-all md:px-4 select-none w-full" onClick={props.onClick}>
-      <div class="h-6">{props.label}</div>
-    </div>
-  );
-};
-
-const ButtonPrimaryDisabled: Component<ButtonVariantProps> = (props) => {
-  return (
-    <div class="box-border py-[10px] bg-purple opacity-75 text-white font-bold text-base rounded-lg flex items-center justify-center cursor-not-allowed hover:opacity-25 transition-all md:px-4 select-none w-full" onClick={props.onClick}>
-      <div class="h-6">{props.label}</div>
-    </div>
-  );
-};
-
-const ButtonSecondaryDefault: Component<ButtonVariantProps> = (props) => {
-  return (
-    <div class="box-border py-[10px] text-purple border border-purple font-bold text-base rounded-lg flex items-center justify-center cursor-pointer hover:bg-purple-hover transition-all md:px-4 select-none w-full" onClick={props.onClick}>
-      <div class="h-6">{props.label}</div>
-    </div>
-  );
-};
-
-const ButtonSecondaryActive: Component<ButtonVariantProps> = (props) => {
-  return (
-    <div class="box-border py-[10px] text-purple border border-purple bg-light-purple font-bold text-base rounded-lg flex items-center justify-center cursor-pointer hover:opacity-25 transition-all md:px-4 select-none w-full" onClick={props.onClick}>
-      <div class="h-6">{props.label}</div>
-    </div>
-  );
-};
-
-const ButtonSecondaryDisabled: Component<ButtonVariantProps> = (props) => {
-  return (
-    <div class="box-border py-[10px] text-purple opacity-25 border border-purple font-bold text-base rounded-lg flex items-center justify-center cursor-not-allowed hover:opacity-25 transition-all md:px-4 select-none w-full" onClick={props.onClick}>
-      <div class="h-6">{props.label}</div>
-    </div>
-  );
 };
 
 export default Button;
